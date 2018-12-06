b56d1379c97a2c5cdf685575fbbe35849918e967
huang.chengwei <foxmail.com>
2018-12-07 01:39:44 +0800

    vue loader

---
+++ src/main/resources/static/weui/main.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="weui-tab">
+        <div class="weui-tab__panel">
+            <router-view></router-view>
+        </div>
+        <div class="weui-tabbar">
+            <router-link to="/main" append class="weui-tabbar__item"
+                         :class="{ 'weui-bar__item_on': isTabbarActive('/main') }">
+                <img src="./images/icon_tabbar.png" alt="" class="weui-tabbar__icon" v-on:click='tabbarActive(0)'>
+                <p class="weui-tabbar__label">主页</p>
+            </router-link>
+            <router-link to="/bar" append class="weui-tabbar__item"
+                         :class="{ 'weui-bar__item_on': isTabbarActive('/bar') }">
+                <img src="./images/icon_tabbar.png" alt="" class="weui-tabbar__icon" v-on:click='tabbarActive(1)'>
+                <p class="weui-tabbar__label">通讯录</p>
+            </router-link>
+            <router-link to="/foo1" append class="weui-tabbar__item"
+                         :class="{ 'weui-bar__item_on': isTabbarActive('/foo1') }">
+                <img src="./images/icon_tabbar.png" alt="" class="weui-tabbar__icon" v-on:click='tabbarActive(2)'>
+                <p class="weui-tabbar__label">发现</p>
+            </router-link>
+            <router-link to="/bar1" append class="weui-tabbar__item"
+                         :class="{ 'weui-bar__item_on': isTabbarActive('/bar1') }">
+                <img src="./images/icon_tabbar.png" alt="" class="weui-tabbar__icon" v-on:click='tabbarActive(3)'>
+                <p class="weui-tabbar__label">我</p>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    register({
+        data: function () {
+            return {
+                tabbar_active_index: this.$router.path
+            }
+        },
+
+        methods: {
+            tabbarActive: function (index) {
+                this.$data.tabbar_active_index = index;
+            },
+            isTabbarActive: function (path) {
+                return this.$route.path === path;
+            }
+        }
+    })
+</script>
---
+++ src/main/resources/static/weui/product.vue
@@ -0,0 +1,86 @@
+<template>
+    <div id='component_product'>
+        <div class="back-round-but">
+            <span @click="$router.go(-1)" class="icon icon-left"></span>
+        </div>
+        <div class='weui-tab__panel'>
+            <div class='product-content'>
+
+                <div class='product-preview'>
+                    <img class="full" src='images/tmp/fm.jpg'>
+                </div>
+                <div class="weui-panel">
+                    <div class="weui-panel__hd">
+                        <h3 style='color: black'>
+                            苹果X手机壳iPhoneXS MAX手机壳男苹果xr手机壳7/8plus防摔潮牌女苹果X透明套xs全包硬壳max镜头保护圈手机壳
+                        </h3>
+                    </div>
+                    <div class="weui-panel__bd">
+                        <div class="weui-media-box weui-media-box_text">
+                            <h4 class="weui-media-box__title real-price">￥99.99</h4>
+                            <ul class="weui-media-box__info weui-flex">
+                                <li class="weui-media-box__info__meta weui-flex__item text-left">
+                                    <span>价格</span>
+                                    <del>￥10.00</del>
+                                </li>
+                                <li class="weui-media-box__info__meta weui-flex__item text-left">快递￥0.00</li>
+                                <li class="weui-media-box__info__meta weui-flex__item text-center">月销量9999</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="weui-loadmore weui-loadmore_line info-title">
+                    <span class="weui-loadmore__tips bg-page">商品详情</span>
+                </div>
+
+                <div class="weui-panel">
+                    <div class="weui-panel__hd">
+                        <div class='weui-media-box weui-media-box_text'>
+                            <img src='images/tmp/fm.jpg'>
+                            <img src='images/tmp/fm.jpg'>
+                            <img src='images/tmp/fm.jpg'>
+                            <img src='images/tmp/fm.jpg'>
+                            <img src='images/tmp/fm.jpg'>
+                            <img src='images/tmp/fm.jpg'>
+                            <img src='images/tmp/fm.jpg'>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="weui-flex weui-tabbar botton-bar">
+            <div class="weui-flex__item text-center cart btn">
+                加入购物车
+            </div>
+            <div class="weui-flex__item text-center buy btn">
+                立即购买
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    var Compoment_product = {
+        name: "compoment-product",
+        data: function () {
+            return {
+                tabbar_active_index: this.$router.path
+            }
+        }
+        ,
+
+        methods: {
+            tabbarActive: function (index) {
+                this.$data.tabbar_active_index = index;
+            }
+            ,
+            isTabbarActive: function (path) {
+                return this.$route.path === path;
+            }
+        }
+    };
+
+    register(Compoment_product)
+</script>
