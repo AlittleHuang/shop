ce99ed37a4853749be34a0652c7b14d1d1de0305
huang.chengwei <foxmail.com>
2018-12-12 19:53:34 +0800

    下班

--- src/main/resources/static/weui/js/config.js
+++ src/main/resources/static/weui/js/config.js
@@ -9,4 +9,16 @@ Vue.filter("toFixed2", function (value) {
     return (1 * value).toFixed(2)
 });
 
+history.pushState(null, null, document.URL);
+
+window.addEventListener('popstate', function (data) {
+    console.log(data)
+    //history.pushState(null, null, document.URL);
+});
+
+window.addEventListener('pushstate', function (data) {
+    console.log(data)
+    //history.pushState(null, null, document.URL);
+});
+
 var Event = new Vue();
